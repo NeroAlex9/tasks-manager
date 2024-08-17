be3c1eb797020b7b84d45144f416daf8a97113be
NeroAlex9 <Nerobelov1997@mail.ru>
2024-08-17 21:17:29 +0300

    update scss task_element

--- src/components/task_element/task_element.tsx
+++ src/components/task_element/task_element.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./task_element.module.scss";
+
+
 
 interface IProps {
   text:string
 }
 
 const TaskElement: React.FC<IProps> = ({text})=> {
+  const [taskDone, setTaskDone] = useState(styles.column__checkbox)
+  const [textDone, setTextDone] = useState(styles.column__text)
+
   return <div className={styles.column}>
-    <input className={styles.column__checkbox} type='radio'/>
-    <p className={styles.column__text}>{text}</p>
-    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div className={taskDone} onClick={()=>{
+      taskDone=== styles.column__checkbox ? setTaskDone(styles.column__checkbox +' '+ styles.column__checkboxDone ) : setTaskDone(styles.column__checkbox)
+      textDone=== styles.column__text ? setTextDone(styles.column__text +' '+ styles.column__textDone ) : setTextDone(styles.column__text)
+
+    }}/>
+    <p className={textDone}>{text}</p>
+    <svg className={styles.column__svg} width="16" height="17" viewBox="0 0 16 17" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
       <line x1="14.0941" y1="1.41108" x2="1.41108" y2="15.9059" stroke="#D75959" stroke-width="2"
             stroke-linecap="round"/>
       <line x1="13.5933" y1="15.8545" x2="1.85448" y2="1.40671" stroke="#D75959" stroke-width="2"
--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./tasks.module.scss";
-import InputTask from "../../components/input_task/Input_task";
+import InputTask from "../../components/task_input/Input_task";
 import TaskElement from "../../components/task_element/task_element";
 
 function Tasks() {
@@ -8,6 +8,7 @@ function Tasks() {
     <div className={styles.column}>
       <InputTask />
       <TaskElement text='Task1' />
+        <TaskElement text='Имеются изображения разных размеров(ширина, высота), которые выводятся с помощью <img src=""> . ' />
     </div>
   );
 }
