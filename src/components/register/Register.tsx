d0b07cd0c1d89c957479bc6364986cf934b4f096
NeroAlex9 <Nerobelov1997@mail.ru>
2024-09-06 17:06:44 +0300

    check repeat password

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -15,31 +15,40 @@ const Register: React.FC<IRegister> = ({ formName, textButton, textLink }) => {
   const dispatch = useDispatch();
   const [login, setLogin] = useState('')
   const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
 useEffect(()=>{
-  
+  console.log(password, repeatPassword)
   dispatch(createLogin(login))
   dispatch(createPassword(password))
-},[dispatch,login,password])
+},[dispatch,login,password,repeatPassword])
 
 
   return (
     <form className={styles.form}>
       <h2 className={styles.form__inlet}>{formName}</h2>
       <input
+      type="string"
   onChange={(e)=>setLogin(e.target.value)}
-  className={styles.input}
+  className={styles.form__input}
   placeholder="Логин"
 />
 <input
+type='password'
 onChange={(e)=>setPassword(e.target.value)}
-  className={styles.input}
+  className={styles.form__input}
    placeholder="Пароль"
 />
 <input
-  className={styles.input}
+type='password'
+onChange={(e)=>setRepeatPassword(e.target.value)}
+  className={styles.form__input}
    placeholder="Повторите пароль"
 />
-      <NavLink to="/tasks">
+<p className = {password==repeatPassword 
+? styles.form__passwordOk
+: styles.form__passwordError
+}>Пароли не совпадают!</p>
+      <NavLink to="/">
         <FormButton onClick={() => {
           dispatch(createUser())
         }} text={textButton} />{" "}
