88aeb75f5bf9750bc6851216e52106a25441d1a3
NeroAlex9 <Nerobelov1997@mail.ru>
2024-09-06 16:19:58 +0300

    add new  login&password to state

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import styles from "./register.module.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch} from "react-redux";
 import { createLogin, createPassword, createUser } from "../../store/slice/registerSlice/registerSlice";
 import { useEffect, useState } from "react";
 import { FormButton } from "../form_button/Form_button";
@@ -16,17 +16,22 @@ const Register: React.FC<IRegister> = ({ formName, textButton, textLink }) => {
   const [login, setLogin] = useState('')
   const [password, setPassword] = useState('')
 useEffect(()=>{
+  
   dispatch(createLogin(login))
   dispatch(createPassword(password))
 },[dispatch,login,password])
+
+
   return (
     <form className={styles.form}>
       <h2 className={styles.form__inlet}>{formName}</h2>
       <input
+  onChange={(e)=>setLogin(e.target.value)}
   className={styles.input}
   placeholder="Логин"
 />
 <input
+onChange={(e)=>setPassword(e.target.value)}
   className={styles.input}
    placeholder="Пароль"
 />
