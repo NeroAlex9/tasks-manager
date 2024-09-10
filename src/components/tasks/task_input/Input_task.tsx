ed39c2e07ddd038751d34a7f04d85df144a84696
NeroAlex9 <Nerobelov1997@mail.ru>
2024-09-10 17:01:04 +0300

    update sccs & clear input

--- src/components/tasks/task_input/Input_task.tsx
+++ src/components/tasks/task_input/Input_task.tsx
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
 import '../../../App'
 import styles from "./input_task.module.scss";
-import type { RootState } from '../../../store/store'
 import  {TaskButton} from "../task_button/Task_button";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 import { addTask, createTask } from "../../../store/slice/tasksSlice/tasksSlice";
 
 
 
 function InputTask() {
-  // const tasksArr = useSelector((state: RootState) => state.tasks)
   const dispatch = useDispatch()
 const[task, setTask]=useState('')
 useEffect(()=>{
-  // console.log(tasksArr)
 dispatch(createTask(task))
 },[dispatch,task])
 
 let addNewTask = ()=>{
-  return ()=>dispatch(addTask())
+  dispatch(addTask());
+  setTask('')
 }
 
   return (
@@ -31,10 +29,11 @@ let addNewTask = ()=>{
         onChange={e=>setTask(e.target.value)}
           className={styles.input__task}
           placeholder="Название задачи"
+          value={task}
         ></input>
         <div className={styles.input__buttons}>
-          <TaskButton onClick={() => {}} color='#D75959' text="Очистить" />
-          <TaskButton onClick={addNewTask()} color='black' text="Добавить" />
+          <TaskButton onClick={()=>setTask('')} color='#D75959' text="Очистить" />
+          <TaskButton onClick={addNewTask} color='black' text="Добавить" />
         </div>
       </div>
     </>
--- src/store/slice/registerSlice/registerSlice.ts
+++ src/store/slice/registerSlice/registerSlice.ts
@@ -14,7 +14,6 @@ const initialState: IRegisterState = {
     people: []
 }
 
-
 export const registerSlice = createSlice({
     name: "Register",
     initialState,
